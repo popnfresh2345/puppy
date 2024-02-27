d4700162bbd2303a5a12542ed21f2a6329501273
Alex Holliday <popnfresh234@gmail.com>
2024-02-27 11:34:36 +0800

    Add big bark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/popnfresh2345/puppy
 
 go 1.22.0
+
+require github.com/popnfresh2345/dog v0.0.0-20240227033137-3ba527997733 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/popnfresh2345/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WHenGrownUp(Barks())
 }
